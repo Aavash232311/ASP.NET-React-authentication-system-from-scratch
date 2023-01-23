c9f47476df6046446d1821e6a97b3d219a8706a5
Aavash Lamichhane <aavash3150@gmail.com>
2023-01-23 23:06:15 +0545

    initial commit

--- ClientApp/src/components/Admin/AdminContext.js
+++ ClientApp/src/components/Admin/AdminContext.js
@@ -5,3 +5,12 @@ const AdminContext = createContext(undefined);
 
 export default AdminContext;
 
+export const AdminProvider = ({children}) => {
+    const methods = {
+        loggedIn: false,
+
+    }
+    return (
+        <AdminContext.Provider value={methods}>{children}</AdminContext.Provider>
+    )
+}
